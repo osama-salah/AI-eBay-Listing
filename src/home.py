22eece49fb93f5e40403e1d050cf67f016aea4db
Osama Salahledin <osama@mx-os>
2025-01-07 01:48:24 +0200

    Used Streamlit's query_params instead of FastAPI

--- src/home.py
+++ src/home.py
@@ -16,44 +16,21 @@ import os
 # Add the project root directory to sys.path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from lib.session import save_session_state, load_session_state, logout
-
 SANDBOX_ENABLE = True
-
-app = FastAPI()
-
-@app.get("/test")
-async def test():
-    return {"status": "Server is running!"}
-
-@app.get("/callback")
-async def callback(code: str):
-    print("Received callback")
-    load_session_state()
-    st.session_state.callback_auth_code = code
-    save_session_state()
-    return RedirectResponse(url="http://localhost:8501")
-
-def run_fastapi():
-    uvicorn.run(app, host="localhost", port=8000)
-    # uvicorn.run(app, host="localhost", port=8000, ssl_keyfile="key.pem", ssl_certfile="cert.pem")
-
-# stop the server
-def stop_server():
-    uvicorn.Server.server_state.should_exit = True
-    st.session_state.server_started = False
-    print("Server stopped")
 
 # Check if navigation_radio is set in session state
 refresh_navigation_radio = st.session_state.navigation_radio if 'navigation_radio' in st.session_state else None
-
 # Load state (without navigation_radio)
 print("Loading session state")
 load_session_state()
-
+# Add after loading session state
+if "code" in st.query_params:
+    st.session_state.callback_auth_code = st.query_params["code"]
+    save_session_state()
+    
 # Restore navigation radio if it exists
 if refresh_navigation_radio:
     st.session_state.navigation_radio = refresh_navigation_radio
-
 # Set the page configuration
 st.set_page_config(page_title="eBay Listing App", page_icon=":mag:", layout="centered")
 
@@ -75,7 +52,6 @@ def navigation_disabled():
 
 def authorize_client(env='production'):
     if not ebay_production.app_token:        
-        # Get production application token
         print(f"Getting production application token...")
         ebay_production.get_app_token()
 
@@ -86,7 +62,6 @@ def authorize_client(env='production'):
             return
 
     if not ebay_sandbox.app_token:
-        # Get sandbox application token
         print(f"Getting sandbox application token...")
         ebay_sandbox.get_app_token(env='sandbox')
 
@@ -96,7 +71,6 @@ def authorize_client(env='production'):
             print("Failed to obtain sandbox application token.")
             return
 
-    # If auth_state is set to authorize, start the authorization process
     if st.session_state.get('auth_state') == 'authorize':
         if env == 'production':
             scopes = ["https://api.ebay.com/oauth/api_scope", "https://api.ebay.com/oauth/api_scope/sell.marketing.readonly", "https://api.ebay.com/oauth/api_scope/sell.marketing", "https://api.ebay.com/oauth/api_scope/sell.inventory.readonly", "https://api.ebay.com/oauth/api_scope/sell.inventory", "https://api.ebay.com/oauth/api_scope/sell.account.readonly", "https://api.ebay.com/oauth/api_scope/sell.account", "https://api.ebay.com/oauth/api_scope/sell.fulfillment.readonly", "https://api.ebay.com/oauth/api_scope/sell.fulfillment", "https://api.ebay.com/oauth/api_scope/sell.analytics.readonly", "https://api.ebay.com/oauth/api_scope/sell.finances", "https://api.ebay.com/oauth/api_scope/sell.payment.dispute", "https://api.ebay.com/oauth/api_scope/commerce.identity.readonly", "https://api.ebay.com/oauth/api_scope/sell.reputation", "https://api.ebay.com/oauth/api_scope/sell.reputation.readonly", "https://api.ebay.com/oauth/api_scope/commerce.notification.subscription", "https://api.ebay.com/oauth/api_scope/commerce.notification.subscription.readonly", "https://api.ebay.com/oauth/api_scope/sell.stores", "https://api.ebay.com/oauth/api_scope/sell.stores.readonly", "https://api.ebay.com/oauth/scope/sell.edelivery"]
@@ -104,21 +78,11 @@ def authorize_client(env='production'):
         elif env == 'sandbox':
             scopes = ["https://api.ebay.com/oauth/api_scope", "https://api.ebay.com/oauth/api_scope/buy.order.readonly", "https://api.ebay.com/oauth/api_scope/buy.guest.order", "https://api.ebay.com/oauth/api_scope/sell.marketing.readonly", "https://api.ebay.com/oauth/api_scope/sell.marketing", "https://api.ebay.com/oauth/api_scope/sell.inventory.readonly", "https://api.ebay.com/oauth/api_scope/sell.inventory", "https://api.ebay.com/oauth/api_scope/sell.account.readonly", "https://api.ebay.com/oauth/api_scope/sell.account", "https://api.ebay.com/oauth/api_scope/sell.fulfillment.readonly", "https://api.ebay.com/oauth/api_scope/sell.fulfillment", "https://api.ebay.com/oauth/api_scope/sell.analytics.readonly", "https://api.ebay.com/oauth/api_scope/sell.marketplace.insights.readonly", "https://api.ebay.com/oauth/api_scope/commerce.catalog.readonly", "https://api.ebay.com/oauth/api_scope/buy.shopping.cart", "https://api.ebay.com/oauth/api_scope/buy.offer.auction", "https://api.ebay.com/oauth/api_scope/commerce.identity.readonly", "https://api.ebay.com/oauth/api_scope/commerce.identity.email.readonly", "https://api.ebay.com/oauth/api_scope/commerce.identity.phone.readonly", "https://api.ebay.com/oauth/api_scope/commerce.identity.address.readonly", "https://api.ebay.com/oauth/api_scope/commerce.identity.name.readonly", "https://api.ebay.com/oauth/api_scope/commerce.identity.status.readonly", "https://api.ebay.com/oauth/api_scope/sell.finances", "https://api.ebay.com/oauth/api_scope/sell.payment.dispute", "https://api.ebay.com/oauth/api_scope/sell.item.draft", "https://api.ebay.com/oauth/api_scope/sell.item", "https://api.ebay.com/oauth/api_scope/sell.reputation", "https://api.ebay.com/oauth/api_scope/sell.reputation.readonly", "https://api.ebay.com/oauth/api_scope/commerce.notification.subscription", "https://api.ebay.com/oauth/api_scope/commerce.notification.subscription.readonly", "https://api.ebay.com/oauth/api_scope/sell.stores", "https://api.ebay.com/oauth/api_scope/sell.stores.readonly"]     
 
-        # Get authorization URL for user consent
         print(f"Getting {env} authorization URL...")
         auth_url = st.session_state.ebay_client.get_auth_url(scopes, env=env)
 
-        # Automatically open the URL in the same tab
-        # webbrowser.open(auth_url)
+        # Open auth URL
         st.markdown(f'<meta http-equiv="refresh" content="0;url={auth_url}">', unsafe_allow_html=True)
-
-        # Start FastAPI server in a separate thread
-        if 'server_started' not in st.session_state:
-            Thread(target=run_fastapi, daemon=True).start()
-            st.session_state.server_started = True                            
-
-            print("FastAPI server started")
-
         st.session_state['auth_state'] = 'auth_waiting'
 
     if st.session_state.get('auth_state') != 'authorized':      
